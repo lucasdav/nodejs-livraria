ee068af10f28949bded9125945b10c1b98c78a49
lucasdav <lucassouzadavanso@gmail.com>
2024-02-05 23:22:34 -0300

    add proj livraria

---
+++ server.js
@@ -0,0 +1,30 @@
+// import http from "http";
+import "dotenv/config";
+import app from "./src/app.js";
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log("servidor escutando");
+});
+
+
+
+// abaixo exemplo com http
+// const http = require("http")
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {"Content-Type": "text/plain"});
+//     res.end(rotas[req.url])
+// });
+
+// abaixo exemplo com htttp
+// const rotas = {
+//     "/": "Curso de Node.js",
+//     "/livros": "Entrei na rota livros",
+//     "/autores": "Entrei na rota autores"
+// };
+
+// abaixo exemplo com http
+// server.listen(PORT, () => {
+//     console.log("servidor escutando");
+// });
---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexao com  o banco feita com sucesso");
+})
+
+const app = express();
+routes(app);
+
+//remover
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    //splice remove o index, em apenas 1 elemento
+    livros.splice(index, 1)
+    res.status(200).send("livro removido com sucesso");
+})
+
+export default app;
