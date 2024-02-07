7d79f5fa22ab1787f3fdd150a0f39f87cc496271
lucasdav <lucassouzadavanso@gmail.com>
2024-02-07 18:38:41 -0300

    update version

--- src/app.js
+++ src/app.js
@@ -1,26 +1,14 @@
 import express from "express";
-import conectaNaDatabase from "./config/dbConnect.js";
+import db from "./config/dbConnect.js";
 import routes from "./routes/index.js";
 
-const conexao = await conectaNaDatabase();
-
-conexao.on("error", (erro) => {
-  console.error("erro de conexão", erro);
-});
-
-conexao.once("open", () => {
-  console.log("Conexao com  o banco feita com sucesso");
+db.on("error", console.log.bind(console, "Erro de conexão"));
+db.once("open", () => {
+  console.log("conexão com o banco feita com sucesso");
 });
 
 const app = express();
+app.use(express.json());
 routes(app);
-
-//remover
-app.delete("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  //splice remove o index, em apenas 1 elemento
-  livros.splice(index, 1);
-  res.status(200).send("livro removido com sucesso");
-});
 
 export default app;
